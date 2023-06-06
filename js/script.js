e389b07a846da1a3b8e457e192e10ad2c812d6be
Nnisarg Gada <gadannisarg@gmail.com>
2023-06-06 22:55:09 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var sidebarTemplate = document.createElement("template");
+
+sidebarTemplate.innerHTML = `
+  <div id="sidebar-layer"></div>
+  <div id="sidebar">
+    <div class="sidebar-icon" onclick="window.location.href='/'">
+      <i class="fa-solid fa-leaf"></i>
+      <span class="sidebar-tip">Home</span>
+      </a>
+    </div>
+    <div class="sidebar-icon" onclick="window.location.href='/play.html'">
+      <i class="fa-solid fa-masks-theater"></i>
+      <span class="sidebar-tip">About the play</span>
+    </div>
+    <div class="sidebar-icon" onclick="window.location.href='/'">
+      <i class="fa-solid fa-user"></i>
+      <span class="sidebar-tip">Cast</span>
+    </div>
+    <div class="sidebar-icon" onclick="window.location.href='/shows.html'">
+      <i class="fa-solid fa-ticket-alt"></i>
+      <span class="sidebar-tip">Book shows</span>
+    </div>
+    <div class="sidebar-icon" onclick="window.location.href='/'">
+      <i class="fa-solid fa-question"></i>
+      <span class="sidebar-tip">About us</span>
+    </div>
+  </div>
+`;
+
+document.body.insertBefore(sidebarTemplate.content, document.body.firstChild);
+
+const sidebar = document.getElementById("sidebar");
+const sidebarOpener = document.getElementById("sidebar-opener");
+const sidebarLayer = document.getElementById("sidebar-layer");
+
+function showSidebar() {
+  sidebar.style.left = "0";
+  sidebar.style.transition = "all 0.5s ease-in-out";
+  sidebarLayer.style.display = "block";
+}
+
+function hideSidebar() {
+  sidebar.style.left = "-100%";
+  sidebar.style.transition = "all 0.5s ease-in-out";
+  sidebarLayer.style.display = "none";
+}
+
+sidebarOpener.addEventListener("click", showSidebar);
+sidebarLayer.addEventListener("click", hideSidebar);
